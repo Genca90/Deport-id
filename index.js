36be706bb868220e75890301df0ab2783884f8c7
Genca90 <161984227+Genca90@users.noreply.github.com>
2024-06-09 20:48:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var video = document.querySelector('.video-config');
+    
+//     // Event listener para detectar cuando el video llega al final
+//     video.addEventListener('ended', function() {
+//       // Reiniciar el video
+//       this.currentTime = 0;
+//       this.play();
+//     });
+//   });
+
+
+
+ 
+
